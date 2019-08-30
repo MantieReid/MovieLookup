a13cb262c9689c2f256169ca82c446b9a5b3c2de
christian-sheridan <christiansheridan@outlook.com>
2019-08-29 19:13:16 -0500

    Added projects

---
+++ src/MovieLookup.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MovieLookup.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MovieLookup.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MovieLookup.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MovieLookup.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5927809-0b46-43c1-8003-387f43b2c709")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
