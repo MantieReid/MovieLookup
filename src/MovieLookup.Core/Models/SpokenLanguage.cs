327f5b08ceb4a44de359e7346d425d420840c618
christian-sheridan <christiansheridan@outlook.com>
2019-08-30 13:59:08 -0500

    Added json response models

---
+++ src/MovieLookup.Core/Models/SpokenLanguage.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MovieLookup.Core.Models
+{
+    public class SpokenLanguage
+    {
+        [JsonProperty("iso_639_1")]
+        public string Iso639_1 { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
